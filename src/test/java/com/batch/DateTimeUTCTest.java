3b25f3b3dc7ff003dd89986dfdde9975c2e8f454
haepyung <haepyung88@gmail.com>
2019-11-05 11:31:35 +0900

    **test3
    Change-Id: I6e6eaa17fea0f0f60ff9ea32fe7cb0e1c36be3c4

--- src/test/java/com/batch/DateTimeUTCTest.java
+++ src/test/java/com/batch/DateTimeUTCTest.java
@@ -7,22 +7,16 @@ public class DateTimeUTCTest
 	@Test
 	public void onTest()
 	{
-		/**
-		 * 제한사항
-			표(board)는 2차원 배열로 주어집니다.
-			표(board)의 행(row)의 크기 : 1,000 이하의 자연수
-			표(board)의 열(column)의 크기 : 1,000 이하의 자연수
-			표(board)의 값은 1또는 0으로만 이루어져 있습니다.	
-		 */
-		int [][]board = {{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,1,0}}; //9
-		
-		// 4 * 4  > 3 * 4 > 3 * 4 >
-		System.out.println("[RESULT] : " + solution(board));
+		int n = 15;
+		System.out.println("[RESULT] : " + solution(n));
 	}
 	
-	public int solution(int [][]board)
+	public int solution(int n)
     {
-        int answer = 1234;
+        int answer = 0;
+        String temp = Integer.toBinaryString(n).replaceAll("0", "");
+        for (int num = n + 1; num <= 1000000; num++)
+        	if (Integer.toBinaryString(num).replaceAll("0", "").equals(temp)) return num;
 
         return answer;
     }
